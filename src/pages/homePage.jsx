b2029aa5ddc505cb5f1c6ef0ac4e1d3c30964a0e
fc115740Dharmasri <maneeshadharmasri@gmail.com>
2026-01-20 23:23:03 +0530

    homepage correction 2

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -10,14 +10,7 @@ export default function HomePage(){
             <div className="w-full min-h-[calc(100%-100px)]">
                 <Routes>
                     <Route path="/" element={<Home/>} />
-                    <Route path="/products" element={<ProductPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                    <Route path="/overview/:productID" element={<ProductOverview/>} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/checkout" element={<CheckoutPage />} />
-                    <Route path="/orders" element={<OrdersPage />} />
-                    <Route path="/*" element={<NotFoundPage />} />
+                    
                 </Routes>
             </div>
         </div>
